70bebad529494cbe2d9b73812fdf06c1d76f2224
TakeMos <kangh0423@gmail.com>
2024-06-15 03:47:54 +0900

    [Setting] 프로젝트 파일 업로드

---
+++ WorkLife/WorkLife/WorkLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkLifeApp.swift
+//  WorkLife
+//
+//  Created by kim kanghyeok on 6/15/24.
+//
+
+import SwiftUI
+
+@main
+struct WorkLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
