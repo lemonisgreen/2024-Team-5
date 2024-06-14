d14ef901a0d434d6ad8a64e44dd8c67f5b12bb87
hyeyeonie <shy020916@naver.com>
2024-06-15 08:17:32 +0900

    [FEAT/#2] 글자 수 세기 및 제한, 사진 촬영 구현 완료

--- WorkLife/WorkLife/PictureDiaryView/PictureDiaryView.swift
+++ WorkLife/WorkLife/PictureDiaryView/PictureDiaryView.swift
@@ -11,6 +11,7 @@ import UIKit
 struct PhotoPicker: UIViewControllerRepresentable {
     @Binding var selectedImage: UIImage?
     @Environment(\.presentationMode) var presentationMode
+    var sourceType: UIImagePickerController.SourceType
     
     class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
         let parent: PhotoPicker
@@ -38,7 +39,7 @@ struct PhotoPicker: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIImagePickerController {
         let picker = UIImagePickerController()
         picker.delegate = context.coordinator
-        picker.sourceType = .photoLibrary
+        picker.sourceType = sourceType
         return picker
     }
     
@@ -47,6 +48,26 @@ struct PhotoPicker: UIViewControllerRepresentable {
     }
 }
 
+//struct CharacterCountView: View {
+//    let maxCount: Int
+//    @Binding var currentCount: Int
+//
+//    var body: some View {
+//        Text("\(currentCount)/\(maxCount)")
+//            .foregroundColor(currentCount > maxCount ? .red : .secondary)
+//            .font(.caption)
+//            .padding(6)
+//            .background(Color.white)
+//            .cornerRadius(8)
+//            .overlay(
+//                RoundedRectangle(cornerRadius: 8)
+//                    .stroke(currentCount > maxCount ? Color.red : Color.secondary, lineWidth: 1)
+//            )
+//            .offset(x: -10, y: -10) // 오른쪽 하단에 위치하도록 offset 적용
+//            .animation(.default)
+//    }
+//}
+
 struct PictureDiaryView: View {
     @State private var birth = Date.now
     @State private var selectedWeather = "sun.max"
@@ -54,7 +75,11 @@ struct PictureDiaryView: View {
     @State private var showingDatePicker = false
     @State private var selectedImage: UIImage? = nil
     @State private var isShowingImagePicker = false
+    @State private var isShowingCameraPicker = false
+    @State private var showImageSourceSelection = false
+    @State private var characterCount = 0
     
+    let maxDiaryLength = 100 // 최대 입력 길이
     let weatherSymbols = ["sun.max", "cloud.sun", "umbrella", "snow"]
     
     let dateFormatter: DateFormatter = {
@@ -69,13 +94,14 @@ struct PictureDiaryView: View {
             HStack {
                 
                 // 날짜
-                HStack {
+                HStack(spacing: 2) {
                     Text(dateFormatter.string(from: birth))
                     
                     Button(action: {
                         showingDatePicker.toggle()
                     }) {
                         Image(systemName: "chevron.down")
+                            .foregroundColor(.blue)
                     }
                 }
                 .popover(isPresented: $showingDatePicker) {
@@ -111,13 +137,13 @@ struct PictureDiaryView: View {
             .padding(.horizontal, 2)
             .padding(.bottom, 20)
             
-            
-            //사진
+            // 사진 추가하기
             Button(action: {
-                isShowingImagePicker.toggle()
+                showImageSourceSelection = true
             }) {
                 ZStack {
                     RoundedRectangle(cornerRadius: 10)
+                        .stroke(style: StrokeStyle(lineWidth: 1))
                         .frame(width: 360, height: 200)
                         .foregroundColor(.secondary)
                     
@@ -128,37 +154,69 @@ struct PictureDiaryView: View {
                             .frame(width: 360, height: 200)
                             .clipped()
                             .cornerRadius(10)
+                        
                     } else {
                         VStack {
                             Image(systemName: "photo")
-                                .font(.system(size: 50))
-                                .foregroundColor(.white)
+                                .font(.system(size: 30))
+                                .foregroundColor(.gray)
                                 .padding(.bottom, 2)
                             Text("사진 추가하기")
                                 .font(.system(size: 14))
-                                .colorMultiply(.black)
+                                .foregroundColor(.black)
                         }
                     }
                 }
             }
-            .sheet(isPresented: $isShowingImagePicker) {
-                PhotoPicker(selectedImage: $selectedImage)
+            .actionSheet(isPresented: $showImageSourceSelection) {
+                ActionSheet(title: Text("사진 추가하기"), buttons: [
+                    .default(Text("사진 앨범에서 선택"), action: {
+                        isShowingImagePicker.toggle()
+                    }),
+                    .default(Text("카메라로 사진 찍기"), action: {
+                        isShowingCameraPicker.toggle()
+                    }),
+                    .cancel()
+                ])
             }
-            .padding(.bottom, 10)
+            .sheet(isPresented: $isShowingImagePicker) {
+                PhotoPicker(selectedImage: $selectedImage, sourceType: .photoLibrary)
+            }
+            .sheet(isPresented: $isShowingCameraPicker) {
+                PhotoPicker(selectedImage: $selectedImage, sourceType: .camera)
+            }
+            .padding(.bottom, 5)
             
             //일기
-            VStack(spacing: 4) {
-                Text("일기 내용")
-                    .padding(.trailing, 290)
-                ZStack {
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(style: /*@START_MENU_TOKEN@*/StrokeStyle()/*@END_MENU_TOKEN@*/)
-                        .foregroundColor(.secondary)
-                    TextEditor(text: $diaryText)
-                        .frame(width: 360, height: 100)
-                    //.border(Color.gray, width: 1)
-                }
+            ZStack {
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(style: StrokeStyle())
+                    .frame(width: 360, height: 130)
+                    .foregroundColor(.secondary)
+                
+                TextField("자랑하고 싶은 내용을 작성하세요", text: $diaryText, axis: .vertical)
+                    .padding(.leading, 5)
+                    .frame(width: 350, height: 110)
+                    .font(.system(size: 16))
+                    .foregroundColor(.black)
+                    .onChange(of: diaryText) { newValue in
+                        if newValue.count > maxDiaryLength {
+                            diaryText = String(newValue.prefix(maxDiaryLength))
+                        }
+                        characterCount = diaryText.count
+                    }
             }
+            Text("\(characterCount)/\(maxDiaryLength)")
+                .foregroundColor(characterCount >= maxDiaryLength ? .red : .secondary)
+                .font(.caption)
+                .padding(6)
+                .background(Color.white)
+                .cornerRadius(8)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(characterCount >= maxDiaryLength ? Color.red : Color.secondary, lineWidth: 1)
+                )
+            
             Spacer()
         }
         .padding()
